a6a74bab1760de59e6f24c2d1b5b3f9e79299dad
Christine Emrich <christine.emrich@web.de>
2019-04-14 16:42:43 +0200

    Make ball move with keys

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [Range(1, 5)]
+    public float speed;
+
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        rigidbody.AddForce(movement * speed);
+    }
+}
